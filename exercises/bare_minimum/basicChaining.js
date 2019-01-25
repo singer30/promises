ff07ad57dcd4a3054a4bd421a8caee70a73d6539
singer30 <lukas.w.singer@gmail.com>
2019-01-24 22:25:52 -0800

    Resolve basicChaining function

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,13 +10,33 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
-
+var pluckFirstLine = require('./promiseConstructor.js');
+var getResponse = require('./promisification.js').getGitHubProfileAsync;
+var writeFileAsync = Promise.promisify(fs.writeFile);
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return pluckFirstLine.pluckFirstLineFromFileAsync(readFilePath)
+    .then((firstLine) => {
+      return getResponse(firstLine);
+    })
+    // .then((profile) => {
+    //   return new Promise( (resolve, reject) => {
+    //     return fs.writeFile(writeFilePath, profile, (err) => {
+    //       if (err) {
+    //         reject(err);
+    //       } else {
+    //         console.log(profile);
+    //         resolve(JSON.stringify(profile));
+    //       }
+    //     })
+    //   })
+    // })
+    .then((response) => {
+      return writeFileAsync(writeFilePath, JSON.stringify(response));
+    });
+     
 };
-
+  
 // Export these functions so we can test them
 module.exports = {
   fetchProfileAndWriteToFile: fetchProfileAndWriteToFile
